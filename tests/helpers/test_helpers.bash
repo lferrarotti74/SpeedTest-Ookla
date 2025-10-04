3e0d94d4d70046ce3556929aa6ea56997b658757
lferrarotti74 <luca@ferrarotti.it>
2025-10-04 22:53:05 +0200

    Update Docker image tag to 'speedtest-ookla:latest' in workflow and test helpers

--- tests/helpers/test_helpers.bash
+++ tests/helpers/test_helpers.bash
@@ -9,7 +9,7 @@ YELLOW='\033[1;33m'
 NC='\033[0m' # No Color
 
 # Docker image name for testing
-TEST_IMAGE="speedtest-test:latest"
+TEST_IMAGE="speedtest-ookla:latest"
 
 # Helper function to check if Docker image exists
 docker_image_exists() {
